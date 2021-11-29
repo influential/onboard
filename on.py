1aabcbc1de7f6cc778da8fad328953dd3a72f3d8
influential <pay10olson@gmail.com>
2021-11-29 01:55:11 -0800

    modularized everything, handled edge cases for file existence

--- on.py
+++ on.py
@@ -3,11 +3,32 @@
 import os, sys, shutil
 from pathlib import Path
 
+
+#    ____        _                         _ 
+#   / __ \      | |                       | |
+#  | |  | |_ __ | |__   ___   __ _ _ __ __| |
+#  | |  | | '_ \| '_ \ / _ \ / _` | '__/ _` |
+#  | |__| | | | | |_) | (_) | (_| | | | (_| |
+#   \____/|_| |_|_.__/ \___/ \__,_|_|  \__,_|
+
+
+def welcome():
+    print("======================================================================================")
+    print("    ____        _                         _ ")                                
+    print("   / __ \      | |                       | |")                                
+    print("  | |  | |_ __ | |__   ___   __ _ _ __ __| |")                                
+    print("  | |  | | '_ \| '_ \ / _ \ / _` | '__/ _` |")                                
+    print("  | |__| | | | | |_) | (_) | (_| | | | (_| |")                                
+    print("   \____/|_| |_|_.__/ \___/ \__,_|_|  \__,_|")                                
+    print()
+    print("======================================================================================")
+
+
 # Copies all of the files inside of the DCIM/###_PANA directory into the specified RAW folder path
-def copyInputToRAW(targetPath):
+def copyInputToRAW(targetPath, inputPath):
     # Set up DCIM path
-    dcimDirectory = os.path.join(inputDrivePath, "DCIM")
-    print(dcimDirectory)
+    dcimDirectory = os.path.join(inputPath, "DCIM")
+    # print(dcimDirectory)
     print()
 
     # Iterate through each data folder inside of DCIM
@@ -15,98 +36,165 @@ def copyInputToRAW(targetPath):
     for folder in dcimFolders:
         # print(folder)
         dataFolderPath = os.path.join(dcimDirectory, folder)
-        print(dataFolderPath)
+        # print(dataFolderPath)
         
         dataFiles = os.listdir(os.path.join(dcimDirectory, folder))
         
         # Copy everything from the input drive into the RAW directory
         for file in dataFiles:
-            print(file)
-            datFilePath = os.path.join(dataFolderPath, file)
-            shutil.copy(datFilePath, targetPath)
-        print()
+            # print(file)
+            dataFilePath = os.path.join(dataFolderPath, file)
+            shutil.copy(dataFilePath, targetPath)
+        # print()
     
-        print("Successfully copied data!")
+        print("Success!")
         print(f"{dataFolderPath} --> {targetPath}")
 
 
-saveDrivePath = '/mnt/i/testingDirVal'
-inputDrivePath = '/mnt/e/testingDirOdin'
-
-print(saveDrivePath)
-print(inputDrivePath)
-print()
-
-saveDriveFiles = os.listdir(saveDrivePath)
-inputDriveFiles = os.listdir(inputDrivePath)
-
-print(f"Save Drive Files:")
-for file in saveDriveFiles:
-    print(file)
-
-print()
-
-print(f"Input Drive Files:")
-for file in inputDriveFiles:
-    print(file)
-
-print()
-print()
-
-
-
 # Determine if the session is for a CONCERT or MUSIC VIDEO
-while True:
-    contentType = int(input("Which option?:\nConcert (1)\nMusic Video (2)\n"))
+def getContentType():
+    while True:
+        contentType = int(input("Which option?:\n   Concert (1)\n   Music Video (2)\n   Exit program (3)\n"))
 
-    if contentType == 1 or contentType == 2:
-        break
-    else:
-        print("\nInvalid Choice - please enter 1 or 2")
+        if contentType == 1 or contentType == 2 or contentType == 3:
+            return contentType
+        else:
+            print("\nInvalid Choice - please enter 1, 2, or 3")
 
 
-# CONCERT
-if contentType == 1:
+def displayDebugInfo(saveDrivePath, inputDrivePath):
+    print(f"Drive we are saving to:\n   {saveDrivePath}")
+    print()
+    print(f"Drive we are pulling from:\n    {inputDrivePath}")
+    print()
 
-    concertName = str(input("\nConcert Title:\n"))
+    saveDriveFiles = os.listdir(saveDrivePath)
+    dcimDirPath = os.path.join(inputDrivePath, "DCIM")
+    inputDriveFolders = os.listdir(dcimDirPath)
 
-    print("Concert")
+    print(f"Save Drive Folders:")
+    for file in saveDriveFiles:
+        print(f"    {file}")
+
+    print()
+
+    print(f"Input Drive Folders:")
+    for folder in inputDriveFolders:
+        # print(file)
+        # print(folder)
+        dataFolderPath = os.path.join(dcimDirPath, folder)
+        print(dataFolderPath)
+        
+        dataFiles = os.listdir(os.path.join(dcimDirPath, folder))
+        
+        # Copy everything from the input drive into the RAW directory
+        for file in dataFiles:
+            print(f"    {file}")
+            # dataFilePath = os.path.join(dataFolderPath, file)
+
+    print()
+    print("======================================================================================")
+    print()
+    print()
+
+
+# Create RAW folder inside of project directory
+def buildRAW(targetPath):
+    rawDirectory = os.path.join(targetPath, "RAW")
+    os.mkdir(rawDirectory)
+    # print(rawDirectory)
     
-    # Determine path of LiveShows and 
-    liveShowsDirPath = os.path.join(saveDrivePath, "LiveShows")
+    return rawDirectory
 
-    # Append new project directory to LiveShows path
-    newProjectDirPath = os.path.join(liveShowsDirPath, concertName)
 
-    print(liveShowsDirPath)
-    print(newProjectDirPath)
+def concert(savePath, inputPath):
+    concertName = str(input("\nEnter concert title:\n"))
+
+    liveShowsDirPath = os.path.join(savePath, "LiveShows")  # Determine path of LiveShows folder
+    newProjectDirPath = os.path.join(liveShowsDirPath, concertName)  # Append new project directory to LiveShows path
+  
+    # print(liveShowsDirPath)
+    # print(newProjectDirPath)
 
     # Create new project folder based on concert name
-    os.mkdir(newProjectDirPath)
-
-    # Create RAW folder inside of project directory
-    rawDirectory = os.path.join(newProjectDirPath, "RAW")
-    os.mkdir(rawDirectory)
-
-    print(rawDirectory)
+    try:
+        os.mkdir(newProjectDirPath)
+    # If the concert somehow already exists, throw an error and exit gracefully
+    except FileExistsError:
+        print(f"Found previous artist folder {newProjectDirPath}")
+        print("Exiting...")
+        quit()
+    
+    # Create the raw directory that stores all of the input data
+    rawDirectory = buildRAW(newProjectDirPath)
 
     # Copy data from input drive into RAW directory
-    copyInputToRAW(rawDirectory)
+    copyInputToRAW(rawDirectory, inputPath)
 
     # Seperate the .MOV files from .JPG and .RW2
 
 
-# MUSIC VIDEO
-elif contentType == 2:
-    print("Music Video")
+
+def musicVid(savePath, inputPath):
+    artistName = str(input("Enter artist name:\n"))
+    songName = str(input("Enter song name:\n"))
+
+    artistsDirPath = os.path.join(savePath, "Artists")  # Determine path of Artists folder
+    newArtistDirPath = os.path.join(artistsDirPath, artistName)  # Append new project directory to Artists path
+    
+    # Try to create a new folder using artist name
+    try:
+        os.mkdir(newArtistDirPath)
+        print(f"Successfully created artist folder {newArtistDirPath}")
+    
+    # If the file already exists its okay, handle the error graciously and continue
+    except FileExistsError:
+        print(f"Found previous artist folder {newArtistDirPath}")
+
+    # print(newArtistDirPath)
+
+    # Create a new project folder inside of the artist's folder
+    newProjectDirPath = os.path.join(newArtistDirPath, songName)
+    
+    # Try to create new folder for the song
+    try:
+        os.mkdir(newProjectDirPath)
+        # print(newProjectDirPath)
+        print(f"Successfully created song folder {newProjectDirPath}")
+
+    # If the song already has a folder, throw an error. Can handle this if we need to later
+    except FileExistsError:
+        print(f"Error - This song folder already exists {newProjectDirPath}")
+        return
+    # Create the raw directory that stores all of the input data
+    rawDirectory = buildRAW(newProjectDirPath)
+
+    # Copy data from input drive into RAW directory
+    copyInputToRAW(rawDirectory, inputPath)
 
 
+def main():
+    # Replace these with environment variables, which will depend on the system it is running on
+    saveDrivePath = '/mnt/i/testingDirVal'
+    inputDrivePath = '/mnt/e/testingDirOdin'
 
+    welcome()
 
+    displayDebugInfo(saveDrivePath, inputDrivePath)
+    mode = getContentType()
 
-else:
-    print("Error - Invalid Content Type")
+    
+    if mode == 1:  # CONCERT
+        concert(saveDrivePath, inputDrivePath)
+    elif mode == 2: # MUSIC VIDEO
+        musicVid(saveDrivePath, inputDrivePath)
+    elif mode == 3:
+        return
+    else:
+        print("Error - Invalid Content Type")
 
+    print()
+    print("Done")
 
-print()
-print("Done")
+if __name__ == "__main__":
+    main()
