2597d744c30cc53aec54b0f003b8d57d15b02d2d
influential <pay10olson@gmail.com>
2021-11-30 03:03:49 -0800

    added sorting by file type into footage and photos folders within RAW

--- on.py
+++ on.py
@@ -24,31 +24,7 @@ def welcome():
     print("===================================================")
 
 
-# Copies all of the files inside of the DCIM/###_PANA directory into the specified RAW folder path
-def copyInputToRAW(targetPath, inputPath):
-    # Set up DCIM path
-    dcimDirectory = os.path.join(inputPath, "DCIM")
-    # print(dcimDirectory)
-    print()
 
-    # Iterate through each data folder inside of DCIM
-    dcimFolders = os.listdir(dcimDirectory)
-    for folder in dcimFolders:
-        # print(folder)
-        dataFolderPath = os.path.join(dcimDirectory, folder)
-        # print(dataFolderPath)
-        
-        dataFiles = os.listdir(os.path.join(dcimDirectory, folder))
-        
-        # Copy everything from the input drive into the RAW directory
-        for file in dataFiles:
-            # print(file)
-            dataFilePath = os.path.join(dataFolderPath, file)
-            shutil.copy(dataFilePath, targetPath)
-        # print()
-    
-        print("Success!")
-        print(f"{dataFolderPath} --> {targetPath}")
 
 
 # Determine if the session is for a CONCERT or MUSIC VIDEO
@@ -98,52 +74,134 @@ def displayDebugInfo(saveDrivePath, inputDrivePath):
     print()
 
 
-# Create RAW folder inside of project directory
-def buildRAW(targetPath):
-    rawDirectory = os.path.join(targetPath, "RAW")
-    os.mkdir(rawDirectory)
-    # print(rawDirectory)
+# Builds a RAW directory at the target path
+def buildRAW(target):
+    # Create the raw directory that stores all of the input data
+    try:
+        rawDirPath = os.path.join(target, "RAW")
+        os.mkdir(rawDirPath)
+    except:
+        print(f"Error making 'RAW' folter at {rawDirPath}")
     
-    return rawDirectory
+    return rawDirPath
+
+# Builds all of the necessary folders per Z's specification
+def buildConcertDirs(concertAbsPath):
+    # Dictionary of paths
+    paths = {}
+
+    # Get the path to the RAW directory 
+    rawDirPath = buildRAW(concertAbsPath)
+    paths['raw'] = rawDirPath
+
+    # Create a photos directory inside of RAW
+    try:
+        photosDirPath = os.path.join(rawDirPath, "photos")
+        os.mkdir(photosDirPath)
+    except:
+        print(f"Error making 'photos' folder at {photosDirPath}")
+        print("Exiting...")
+        quit()
+    paths["photos"] = photosDirPath
+
+    try:
+        footageDirPath = os.path.join(rawDirPath, "footage")
+        os.mkdir(footageDirPath)
+    except:
+        print(f"Error making 'footage' folder at {footageDirPath}")
+        print("Exiting...")
+        quit()
+    paths["footage"] = footageDirPath
+
+    try:
+        toEditDirPath = os.path.join(photosDirPath, "To Edit")
+        os.mkdir(toEditDirPath)
+    except:
+        print(f"Error making 'To Edit' folder at {toEditDirPath}")
+        print("Exiting...")
+        quit()
+    paths["toEdit"] = toEditDirPath
+    return paths
+
+# Copies all of the files inside of the DCIM/###_PANA directory into the specified RAW folder path
+# TODO: NEED TO CHECK THAT FILES ARE NOT FROM OLD IMPORT BASED ON DATE <-- Get INPUT
+
+def copyInputToRAW(paths, inputPath):
+    # Set up DCIM path
+    dcimDirectory = os.path.join(inputPath, "DCIM")
+    # print(dcimDirectory)
+    print()
+
+    # Iterate through each data folder inside of DCIM
+    dcimFolders = os.listdir(dcimDirectory)
+    for folder in dcimFolders:
+        # print(folder)
+        dataFolderPath = os.path.join(dcimDirectory, folder)
+        # print(dataFolderPath)
+        
+        dataFiles = os.listdir(os.path.join(dcimDirectory, folder))
+        
+        # Copy everything from the input drive into the appropriate directory
+        for file in dataFiles:
+            try:
+                extension = file.split('.')[1]
+            except:
+                print(f"Error getting extension of file {file}")
+                print("Exiting...")
+                quit()    
+           
+            print(extension)
+             
+            if extension == 'mov' or extension == 'MOV':
+                copyToTargetPath(paths['footage'], dataFolderPath, file)
+            elif extension == 'jpg' or extension == 'JPG' or extension == 'jpeg' or extension == 'JPEG' or extension == 'rw2' or extension == 'RW2':
+                copyToTargetPath(paths['photos'], dataFolderPath, file)
+            elif extension == "txt" or extension == 'TXT':
+                copyToTargetPath(paths['photos'], dataFolderPath, file)
+            else:
+                print(f"Invalid file type found {extension}")
+                print("Exiting...")
+                quit()
+            
+            
+        # print()
+    
+        
+
+# Copy the input file to the specified target path
+def copyToTargetPath(targetPath, inputFolderPath, file):
+    inputFilePath = os.path.join(inputFolderPath, file)
+    shutil.copy(inputFilePath, targetPath) 
+    print("Success!")
+    print(f"{inputFilePath} --> {targetPath}")
 
 
+# Handles all of the logic for concert sessions
 def concert(savePath, inputPath):
     concertName = str(input("\nEnter concert title:\n"))
 
     liveShowsDirPath = os.path.join(savePath, "LiveShows")  # Determine path of LiveShows folder
-    newProjectDirPath = os.path.join(liveShowsDirPath, concertName)  # Append new project directory to LiveShows path
   
     # print(liveShowsDirPath)
     # print(newProjectDirPath)
 
     # Create new project folder based on concert name
     try:
+        newProjectDirPath = os.path.join(liveShowsDirPath, concertName)  # Append new project directory to LiveShows path
         os.mkdir(newProjectDirPath)
+    
     # If the concert somehow already exists, throw an error and exit gracefully
     except FileExistsError:
         print(f"Found previous artist folder {newProjectDirPath}")
         print("Exiting...")
         quit()
     
-    # Create the raw directory that stores all of the input data
-    rawDirectory = buildRAW(newProjectDirPath)
+    # Create all of the necessary sub-directories inside of the concert folder
+    pathsDict = buildConcertDirs(newProjectDirPath)
 
     # Copy data from input drive into RAW directory
-    copyInputToRAW(rawDirectory, inputPath)
+    copyInputToRAW(pathsDict, inputPath)
 
-    # Seperate the .MOV files from .JPG and .RW2
-
-    # Create a photos directory inside of RAW
-
-    # Store all .JPG and .RW2 files in photos directory
-
-    # Create an empty 'To Edit' directory inside of the photos directory
-
-    # Create a footage directory inside of RAW
-
-    # Store all .MOV files inside of footage directory
-
-    # Done
 
 def musicVid(savePath, inputPath):
     artistName = str(input("Enter artist name:\n"))
@@ -199,11 +257,11 @@ def main():
     mode = getContentType()
 
     if mode == 1:  # CONCERT
-        # concert(saveDrivePath, inputDrivePath)
-        concert(windowsSaveDrivePath, windowsInputDrivePath)
+        concert(saveDrivePath, inputDrivePath)
+        # concert(windowsSaveDrivePath, windowsInputDrivePath)
     elif mode == 2: # MUSIC VIDEO
-        # musicVid(saveDrivePath, inputDrivePath)
-        musicVid(windowsSaveDrivePath, windowsInputDrivePath)
+        musicVid(saveDrivePath, inputDrivePath)
+        # musicVid(windowsSaveDrivePath, windowsInputDrivePath)
     elif mode == 3:
         return
     else:
